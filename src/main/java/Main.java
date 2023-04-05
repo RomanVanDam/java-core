9b7edcc84bf6d08e96993d9ca0c3ff5a92de45a4
Roman Lozinskiy <romanonetap@yandex.ru>
2023-04-05 17:09:40 +0300

    Выполненное задание

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        FrequencyCounter counter = new FrequencyCounter("War and Piece.txt");
+        counter.printStat();
+    }
+}
